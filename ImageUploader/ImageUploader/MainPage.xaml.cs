4e07aab3d4a843b0f959bdf4c50c1cb97918ed93
sourcecode121 <sourcecode121@users.noreply.github.com>
2018-07-23 20:25:56 +0530

    Add project files.

---
+++ ImageUploader/ImageUploader/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageUploader
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        async void SelectImageButton_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if(!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Error", "This is not supported on your device", "Ok");
+                return;
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            if(selectedImageFile == null)
+            {
+                await DisplayAlert("Error", "There was an error when trying to get your image, please try again", "Ok");
+                return;
+            }
+
+            selectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+
+            UploadImage(selectedImageFile.GetStream());
+        }
+
+        private async void UploadImage(Stream stream)
+        {
+            var account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=imagestoragexamarin;AccountKey=MXMvHuy2sb0vpnhnt9jGEApLPwEW5Igtn8U3jZbxmJ+h++bgqV+pzA9YPNxNRPXBlJ435RBPDlroK3Vk8r8nPg==;EndpointSuffix=core.windows.net");
+            var client = account.CreateCloudBlobClient();
+            var container = client.GetContainerReference("imagecontainer");
+            await container.CreateIfNotExistsAsync();
+
+            var name = Guid.NewGuid().ToString();
+            var blockBlob = container.GetBlockBlobReference($"{name}.jpg");
+            await blockBlob.UploadFromStreamAsync(stream);
+
+            string url = blockBlob.Uri.OriginalString;
+        }
+    }
+}
